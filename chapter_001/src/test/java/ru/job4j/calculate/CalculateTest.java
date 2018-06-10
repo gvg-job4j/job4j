1e3d3e5b6de0a525c5e0a346c92a501a2e739f8d
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-10 15:29:45 +0300

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/*
+*Test for Calculate
+*@author Valeriy Gyrievskikh
+*@since 10.06.2018
+*/
+public class CalculateTest{
+	
+	/**
+	*Test echo
+	*/
+	@Test
+	public void whenTakeNameThenThreeEchoPlusName(){
+		String input = "Valeriy";
+		String expect = "Echo, echo, echo : Valeriy";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+}
