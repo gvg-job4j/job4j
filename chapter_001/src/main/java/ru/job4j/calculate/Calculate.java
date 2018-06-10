16ec145005a9c85935f7fbf5d07cea01e3b31049
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-10 14:53:27 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+*Class Calculate
+*@author Valeriy Gyrievskikh
+*@since 09.06.2018
+*/
 public class Calculate{
+	
+	/**
+	*Вывод строки на консоль.
+	*@param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Valeriy Gyrievskikh
+* @since 10.06.2018
+*/
+package ru.job4j.calculate;
