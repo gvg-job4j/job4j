a5c271329190e3c2f35ae598bf413410ab89285f
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-11 17:56:01 +0300

    2.2. Импорт Maven проекта.

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.calculate;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -10,16 +11,16 @@ import static org.junit.Assert.assertThat;
 *@since 10.06.2018
 */
 public class CalculateTest {
-	
-	/**
-	*Test echo
-	*/
-	@Test
-	public void whenTakeNameThenThreeEchoPlusName() {
-		String input = "Valeriy";
-		String expect = "Echo, echo, echo : Valeriy";
-		Calculate calc = new Calculate();
-		String result = calc.echo(input);
-		assertThat(result, is(expect));
-	}
+
+    /**
+     * Test echo
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Valeriy";
+        String expect = "Echo, echo, echo : Valeriy";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 }
