e0ac380cccf458eee2660a210b402df34840878e
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-12 22:38:27 +0300

    4.2. Максимум из двух чисел.

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.max;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 12.06.2018.
+ */
+public class Max {
+
+    /**
+     * Вовзращает максимальное из двух целых чисел
+     * @param first Первое число.
+     * @param second Второе число.
+     * @return максимальное значение.
+     */
+    public int max(int first, int second) {
+        return (first > second ? first : second);
+    }
+
+    ;
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 12.06.2018.
+ */
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirstMoreSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenFirstEqualSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 3);
+        assertThat(result, is(3));
+    }
+}
