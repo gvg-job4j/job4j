3d31c02b9b3c9dad0c9f5fc9b92c34620ed77a05
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-12 21:01:19 +0300

    4.1. Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 12.06.2018.
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     *
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
