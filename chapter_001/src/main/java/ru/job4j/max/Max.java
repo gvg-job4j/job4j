28184e8192412eb8f343c4ef4a63aea17935ce31
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-13 23:25:43 +0300

    Убрал temp.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -26,8 +26,6 @@ public class Max {
      * @return Максимальное значение.
      */
     public int max(int first, int second, int third) {
-        int temp = this.max(first, second);
-        temp = this.max(temp, third);
-        return temp;
+        return this.max(this.max(first, second), third);
     }
 }
