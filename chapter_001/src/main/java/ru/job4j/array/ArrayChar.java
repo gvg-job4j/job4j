963991b1ac4741e7d63eb6e82122fa9bb225fc4e
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-14 22:41:39 +0300

    6.4. Слова начинается с ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 14.06.2018.
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     *
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] != data[i]){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
