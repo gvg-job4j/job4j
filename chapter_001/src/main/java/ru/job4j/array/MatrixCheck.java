97e742185e92baec069b6088717a52845d4f5e01
gvg-job4j <gvgjava2018@yandex.ru>
2018-06-14 23:28:58 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+/**
+ * @author Valeriy Gyrievskikh
+ * @since 14.06.2018.
+ */
+public class MatrixCheck {
+    /**
+     * Проверяет, одинаковы ли элементы в диагоналях массива.
+     *
+     * @param data Проверяемый массив.
+     * @return Результат проверки.
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        boolean value = data[0][0];
+        for (int i = 0; i < data.length; i++) {
+            if (data[i][i] != value) {
+                result = false;
+                break;
+            }
+        }
+        if (result != false) {
+            value = data[0][data.length - 1];
+            for (int i = 0; i < data.length; i++) {
+                if (data[i][data.length - 1 - i] != value) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
