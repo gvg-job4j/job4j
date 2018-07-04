a929dd0301ef54bc7ca64ae9ae7006847ba55059
gvg-job4j <gvgjava2018@yandex.ru>
2018-07-05 00:47:34 +0300

    1. Рефакторинг - Перенести общие методы в абстрактный класс.

---
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
@@ -0,0 +1,55 @@
+package ru.job4j.coffee;
+
+/**
+ * @author Valeriy Gyrievskikh.
+ * @since 04.07.2018.
+ */
+public class CoffeeMachine {
+    /**
+     * Массив доступных номиналов для сдачи.
+     */
+    private int[] money = new int[]{1, 2, 5, 10};
+
+    /**
+     * Метод возвращает сдачу.
+     *
+     * @param value Заплаченная сумма.
+     * @param price Стоимость выбранного напитка.
+     * @return Массив номиналов на сумму сдачи.
+     */
+    int[] changes(int value, int price) {
+        int[] moneyBack = new int[]{0};
+        int change = value - price;
+        if (change > 0) {
+            int[] countBack = new int[]{0, 0, 0, 0};
+            int length = money.length;
+            while (change > 0) {
+                if (change / money[length - 1] > 0) {
+                    countBack[length - 1] = change / money[length - 1];
+                }
+                change -= (change / money[length - 1]) * money[length - 1];
+                length--;
+            }
+            int count = 0;
+            for (int i = 0; i < countBack.length; i++) {
+                if (countBack[i] != 0) {
+                    count += countBack[i];
+                }
+            }
+            if (count != 0) {
+                moneyBack = new int[count];
+                count = 0;
+                for (int i = countBack.length - 1; i > 0; i--) {
+                    if (countBack[i] != 0) {
+                        for (int j = 0; j < countBack[i]; j++) {
+                            moneyBack[count] = money[i];
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return moneyBack;
+    }
+}
+
---
+++ chapter_002/src/test/java/ru/job4j/coffee/CoffeeMachineTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.coffee;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Valeriy Gyrievskikh.
+ * @since 04.07.2018.
+ */
+public class CoffeeMachineTest {
+
+    @Test
+    public void whenMoneyNotEqualCostOn15(){
+        CoffeeMachine machine = new CoffeeMachine();
+        int[] result = machine.changes(50, 35);
+        int[] expect = new int[]{10, 5};
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMoneyNotEqualCostOn65(){
+        CoffeeMachine machine = new CoffeeMachine();
+        int[] result = machine.changes(100, 35);
+        int[] expect = new int[]{10, 10, 10, 10, 10, 10, 5};
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMoneyNotEqualCostOn5(){
+        CoffeeMachine machine = new CoffeeMachine();
+        int[] result = machine.changes(10000, 9995);
+        int[] expect = new int[]{5};
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMoneyEqualCost(){
+        CoffeeMachine machine = new CoffeeMachine();
+        int[] result = machine.changes(50, 50);
+        int[] expect = new int[]{0};
+        Assert.assertThat(result, is(expect));
+    }
+}
