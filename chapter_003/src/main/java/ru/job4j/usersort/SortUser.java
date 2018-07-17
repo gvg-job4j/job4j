995ee7c6b06817db21061c1c69dcca4c2926c258
gvg-job4j <gvgjava2018@yandex.ru>
2018-07-17 23:37:34 +0300

    2. Сортировка User с использованием Comparator.

--- chapter_003/src/main/java/ru/job4j/usersort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/usersort/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.usersort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author Valeriy Gyrievskikh
@@ -17,5 +15,53 @@ public class SortUser {
      */
     public Set<User> sort(List<User> list) {
         return new TreeSet<>(list);
+    }
+
+    /**
+     * Метод сортирует список пользователей по длине имени.
+     *
+     * @param list Список пользователей.
+     * @return Отсортированный список пользователей.
+     */
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(new NameComparator());
+        return list;
+    }
+
+    /**
+     * Метод сортирует список пользователей по имени и возрасту.
+     *
+     * @param list Список пользователей.
+     * @return Отсортированный список пользователей.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(new AgeNameComparator());
+        return list;
+    }
+
+    /**
+     * Метод выполняет сортировку по длине имени.
+     */
+    private class NameComparator implements Comparator<User> {
+        @Override
+        public int compare(User userFirst, User userSecond) {
+            int lengthFirst = userFirst.getName().toCharArray().length;
+            int lengthSecond = userSecond.getName().toCharArray().length;
+            return lengthFirst - lengthSecond;
+        }
+    }
+
+    /**
+     * Метод выполняет сортировку по имени и возрасту.
+     */
+    private class AgeNameComparator implements Comparator<User> {
+        @Override
+        public int compare(User userFirst, User userSecond) {
+            int result = userFirst.getName().compareToIgnoreCase(userSecond.getName());
+            if (result == 0) {
+                result = Integer.compare(userFirst.getAge(), userSecond.getAge());
+            }
+            return result;
+        }
     }
 }
--- chapter_003/src/test/java/ru/job4j/usersort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/usersort/SortUserTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.core.Is.is;
  * @since 16.07.2018
  */
 public class SortUserTest {
+
     @Test
     public void whenListThenSet() {
         List<User> list = new ArrayList<>();
@@ -26,12 +27,51 @@ public class SortUserTest {
         Set<User> sortSet = sortUser.sort(list);
         int[] result = new int[4];
         int count = 0;
-        for (User user : sortSet
-                ) {
+        for (User user : sortSet) {
             result[count] = user.getAge();
             count++;
         }
         int[] expect = new int[]{15, 30, 31, 39};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenListThenListSortetByNameLength() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("Abbybas", 31));
+        list.add(new User("Z", 39));
+        list.add(new User("Vovik", 15));
+        list.add(new User("Vova", 30));
+
+        SortUser sortUser = new SortUser();
+        List<User> sortList = sortUser.sortNameLength(list);
+        int[] result = new int[4];
+        int count = 0;
+        for (User user : sortList) {
+            result[count] = user.getAge();
+            count++;
+        }
+        int[] expect = new int[]{39, 30, 15, 31};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenListThenListSortetByNameAge() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("Abb", 31));
+        list.add(new User("Vovik", 39));
+        list.add(new User("Vovik", 15));
+        list.add(new User("Abb", 30));
+
+        SortUser sortUser = new SortUser();
+        List<User> sortList = sortUser.sortByAllFields(list);
+        int[] result = new int[4];
+        int count = 0;
+        for (User user : sortList) {
+            result[count] = user.getAge();
+            count++;
+        }
+        int[] expect = new int[]{30, 31, 15, 39};
         assertThat(result, is(expect));
     }
 
